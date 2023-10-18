a98221a310e1bf4f71d23bac3fe048d0522de9fb
SamuelSolomon1 <sjnsolomon01@gmail.com>
2023-10-19 00:38:01 +0800

    added ammenities

---
+++ pages/ammenities.php
@@ -0,0 +1,92 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="/CSS/ammenities.css">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    
+
+    <main>
+        <div class="wrap">
+    <div class="container">
+        <h2 id="box-text">SPORTS FACILITIES</h2>
+  <div class="row">
+    <!-- row 1 starts -->
+    <div class="col-md-4" id="box">
+      <div class="card" >
+        <img src="/images/sports facilities/olympic size pool.jpg" class="card-img-top" alt="Card Image 1">
+        <div class="card-body">
+          <h5 class="card-title">Olympic Size Swimming Pool</h5>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4" id="box">
+      <div class="card">
+        <img src="/images/sports facilities/tennis court1.jpg" class="card-img-top" alt="Card Image 2">
+        <div class="card-body">
+          <h5 class="card-title">Indoor Tennis Court</h5>   
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4" id="box">
+      <div class="card">
+        <img src="/images/sports facilities/basketball court1.jpg" class="card-img-top" alt="Card Image 3">
+        <div class="card-body">
+          <h5 class="card-title">Indoor Basketball Court</h5>
+        </div>
+      </div>
+    </div></br><br/><br/>
+    <!-- row 1 ends -->
+    <!-- row 2 starts -->
+     <h2 id="box-text">FUNCTION ROOMS</h2>
+    <div class="col-md-4" id="box">
+      <div class="card">
+        <img src="/images/amenities/socialhall.jpg" class="card-img-top" alt="Card Image 1">
+        <div class="card-body">
+          <h5 class="card-title">Social Lounge</h5>
+          <p>Air conditioning</p>
+          <p>Full bar</p>
+          <p>60 pax full capacity</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4" id="box">
+      <div class="card">
+        <img src="/images/amenities/conferencehall.jpg" class="card-img-top" alt="Card Image 2">
+        <div class="card-body">
+          <h5 class="card-title">Conference Hall</h5>   
+           <p>Air conditioning</p>
+          <p>Mircrophone & Sound System</p>
+          <p>50 pax full capacity</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4" id="box">
+      <div class="card">
+        <img src="/images/amenities/eventhall.jpg" class="card-img-top" alt="Card Image 3">
+        <div class="card-body">
+          <h5 class="card-title">Event Hall</h5>
+           <p>Air conditioning</p>
+          <p>Full bar</p>
+          <p>100 pax full capacity</p>
+        </div>
+      </div>
+    </div>
+
+
+    </div>  <!--  div class wrap-->
+  </div>
+</div>
+</main>
+
+<!-- script -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+</html>
