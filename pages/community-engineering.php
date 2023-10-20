b0e6e9d8241f13ea87c59f86e6d957266417046e
SamuelSolomon1 <sjnsolomon01@gmail.com>
2023-10-20 17:08:23 +0800

    added scroll to top

---
+++ pages/community-engineering.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Community-finance</title>
+    <link rel="stylesheet" href="../CSS/community.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+    <!-- navbar starts -->
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="main.html">
+                <img src="../images/logo/brandlogowhite.png" alt="Logo" id="navbar-logo">
+                LUMINARY TOWERS
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.html">ABOUT</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="services.html">PROPERTIES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="booking.html">AMMENTIES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.html">CONTACT</a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- navbar ends -->
+    <main>
+        <section>
+            <div class="box1">
+                <div class="container box">
+                    <h1>General Provisions</h1><br>
+                    <p class="box-padding">
+
+                        1.1 These House Rules and Regulations (RULES), pursuant to the Master Deed with Declaration Restrictions of Luminary Towers (MASTER DEED), shall govern and regulate the use,<br>
+                        occupancy and enforcement of the individual units, common areas, and limited common areas of the Condominium, to ensure their efficient and orderly management and to promote <br>
+                        the common good of the unit owners and residents of Luminary Towers. <br><br>
+
+                        1.2 All unit owners are required to comply with these RULES which must likewise be observed by (a) members of their family and their household help; (b) their lessees and members <br>
+                        of the family and household help of said lessees; (c) their guest and guests of their lessees; (d) those transacting business with them, or visiting the Condominium. <br><br>
+
+                        1.3 The Condominium Corporation or its duly authorized representative shall have full authority to implement these RULES which are a supplement to the bylaws and MASTER DEED <br>
+                        and should be interpreted in relation there to. An authorized representative refers to any of the officers of the Condominium Corporation so empowered or the duly appointed <br>
+                        Building Administration. Failure of the Condominium Corporation to enforce a rule or regulation or to render such determination shall in no way be construed as a waiver or an <br><br>
+                        abandonment of said rule or regulation under any future application.
+
+                        1.4 The Unit Owner may only be able to keep an animal or pet within the residential unit within Luminary Towers subject to compliance with the limitations and conditions prescribed <br>
+                        in the RULES.
+
+                        1.5 Each Unit Owner, occupant and/or tenant is obliged to keep and maintain his unit in a good tenant-able condition, a state of preservation and cleanliness and in a manner that will <br>
+                        not cause any prejudice or damage to other units/or the common areas of Luminary Towers.</p>
+                </div>
+            </div> <!--box1 ends-->
+            <!-- card navigation starts -->
+            <div class="container bottom-box">
+                <div class="card text-center">
+                    <div class="card-header">
+                        <ul class="nav nav-tabs card-header-tabs">
+                            <li class="nav-item">
+                                <!--add  <a class="nav-link active" aria-current="true" href="#"> for active tab-->
+                                <a class="nav-link " href="/pages/community.php">Administrative</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/pages/community-billing.php">Billing & Collection</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="true" href="/pages/community-engineering.php">Engineering</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Quarterly Preventive Maintenance of Generator</h5>
+                        <hr>
+                        <p class="card-text">In line with our continuous effort to maintain our Electrical System and Equipment, please be advised that we will be conducting <br>
+                            our Quarterly Preventive Maintenance of our Generators and Automatic Transfer Switch (ATS) Simulation where there will be an <br>
+                            expected power fluctuation due to the transfer of Normal Power to Emergency Power and vice versa. <br><br>
+
+                            The purpose of the activity is to proactively check the functionality of the Generator and ATS in case of emergency.<br>
+
+                            Kindly see below the schedule for reference:</p>
+
+                        <!-- table starts-->
+                        <div class="container">
+                            <table class="table table-bordered">
+                                <tbody>
+                                    <tr>
+                                        <td>Date and Time</td>
+                                        <td>Area</td>
+                                        <td>Activity</td>
+                                    </tr>
+                                    <tr>
+                                        <td>June 20, 2022 (10:00 AM to 10:15 AM)</td>
+                                        <td>All Floors (Tower 1)</td>
+                                        <td>ATS Simulation and Load Testing</td>
+                                    </tr>
+                                    <tr>
+                                        <td>June 20, 2022 (10:15 AM to 10:30 AM)</td>
+                                        <td>All Floors (Tower 2)</td>
+                                        <td>ATS Simulation and Load Testing</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- table ends -->
+                        <p class="card-text">For queries and clarification, you may contact the Property Management Office at (02) 8668-8124, (02) 7713-4364 or send us <br>
+                            an email at luminary.admin@luminarytowers.ph or lma@luminaryresidences.ph.</p>
+                        <p class="card-text">We apologize for the inconvenience and hope for your kind understanding. Thank you for your usual cooperation.</p>
+                    </div>
+                </div>
+            </div> <!-- div container ends -->
+            <!-- card navigation ends -->
+        </section>
+        <!-- scroll up -->
+        <section><button type="button" class="btn btn-danger btn-floating btn-lg" id="btn-back-to-top">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z" />
+                </svg>
+            </button>
+        </section>
+        <!-- scroll up ends -->
+    </main>
+    <!-- footer starts -->
+    <footer class="custom-footer py-4 mt-4">
+        <div class="container">
+            <div class="row">
+                <!-- Column 1 -->
+                <div class="col-lg-4 col-md-6 mb-4 mb-lg-0 text-white">
+                    <strong style="color: #ffff;">BUSINESS HOURS</strong><br>
+                    Monday to Friday: 9:00 AM - 12:00 PM, 1:00 PM - 6:00 PMSaturday: 9:00 AM - 1:00 PM
+                    Sunday and holidays: Closed</a><br><br>
+                    <strong style="color: #ffff;">ADDRESS</strong><br>
+                    2/F Tower 1, Lumina Tower 31st Ave, BGC Taguig City, Philippines<br>
+                </div>
+                <!-- Column 2 -->
+                <div class="col-lg-4 col-md-6 mb-4 mb-lg-0 text-white">
+                    <strong style="color: #ffff;">YOU MAY REACH US</strong><br>
+                    Admin: (02) 7718-4364Engineering: (02) 7000-2381
+                    Engineering: (02) 7000-2381
+                    Billing and Collection: (02) 8711-5598,(0998) 936-3766 (Smart), (0967) 203-9054 (Globe) <br>
+                </div>
+                <!-- Column 3 -->
+                <div class="col-lg-4 col-md-6 text-white">
+                    <!-- <img src="../images/logo/Untitled design (6).png" alt="" id="footerLogo"> -->
+                    <strong style="color: #ffff;">CUSTOMER SERVICE</strong><br>
+                    SM Development CorporationG/F Tower A, Two E-Com CenterOcean Drive, Mall Of Asia ComplexPasay City, 1300, PhilippinesPhone: (02) 8858-0300Email: customerservice@smdevelopment.com.<br>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <div class="col-lg-12 col-md-6 mb-4 mb-lg-0 text-white text-center" id="copyRight">
+        <div id="copyNote">
+            &copy; 2016 - 2023 Luminary Towers Condominium Corporation. All rights reserved.
+        </div>
+    </div>
+    <!-- footer ends -->
+    <!-- sroll to top -->
+    <script>
+        let mybutton = document.getElementById("btn-back-to-top");
+        window.onscroll = function() {
+            scrollFunction();
+        };
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                mybutton.style.display = "block";
+            } else {
+                mybutton.style.display = "none";
+            }
+        }
+        mybutton.addEventListener("click", backToTop);
+
+        function backToTop() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
+    </script>
+    <!--bootstrap script -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+
+</html>
